477fd2b81daa9ebf2de1834fad80fe9b4930855a
chayveng <33687510+chayveng@users.noreply.github.com>
2026-04-18 13:21:10 +0700

    Add additional packages to install-app.sh

--- script/install-app.sh
+++ script/install-app.sh
@@ -1,3 +1,4 @@
+# /bin/bash
 sudo apt update && sudo apt install -y \
 vim \
 btop \
@@ -5,3 +6,4 @@ tty-clock \
 net-tools \
 openconnect \
 openvpn 
+sudo apt install tmux zsh fzf ripgrep bat eza fd-find jq btop
